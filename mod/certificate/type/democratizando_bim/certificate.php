31e2c9da6b76b65a5a79587719d9f8b5e30b2542
Inty Castillo <inty.castillo@qisat.com.br>
2020-10-26 14:52:16 -0300

    Certificado - versão final
    Certificado - versão final

--- mod/certificate/type/democratizando_bim/certificate.php
+++ mod/certificate/type/democratizando_bim/certificate.php
@@ -90,7 +90,7 @@ certificate_print_image($pdf, $certificate, CERT_IMAGE_SIGNATURE, $sigx, $sigy, 
 
 // Add text
 $pdf->SetTextColor(20, 20, 20);
-certificate_print_text($pdf, $x, $y + 63, 'C', $fontsans, 'BI', 30, fullname($USER));
+certificate_print_text($pdf, $x, $y + 60, 'C', $fontsans, 'I', 30, fullname($USER));
 certificate_print_text($pdf, $x, $y + 82, 'C', $fontsans, 'B', 12, format_string($course->fullname));
 certificate_print_text($pdf, $x, $y + 92, 'C', $fontsans, '', 14,  certificate_get_date($certificate, $certrecord, $course));
 certificate_print_text($pdf, $x, $y + 102, 'C', $fontserif, '', 10, certificate_get_grade($certificate, $course));
