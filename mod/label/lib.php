af7800148b8600b0f786d55bd428ba33cb174483
Inty Castillo <inty.castillo@qisat.com.br>
2020-10-28 08:05:45 -0300

    Mod label Complete Automatico
    Mod label Complete Automatico

--- mod/label/lib.php
+++ mod/label/lib.php
@@ -180,7 +180,7 @@ function label_supports($feature) {
         case FEATURE_GROUPS:                  return false;
         case FEATURE_GROUPINGS:               return false;
         case FEATURE_MOD_INTRO:               return true;
-        case FEATURE_COMPLETION_TRACKS_VIEWS: return false;
+        case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
         case FEATURE_GRADE_HAS_GRADE:         return false;
         case FEATURE_GRADE_OUTCOMES:          return false;
         case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
