fb76130f7b6bcb59e5259b5edb78c4052dbe90e6
Inty Castillo <inty.castillo@qisat.com.br>
2020-11-05 16:41:19 -0300

    Correção no certificado
    Correção no certificado

--- mod/certificate/type/democratizando_bim/certificate.php
+++ mod/certificate/type/democratizando_bim/certificate.php
@@ -100,7 +100,7 @@ $sql = "SELECT MAX(c.timecompleted) as timecompleted
                    AND c.course = :courseid";
 $timecompleted = $DB->get_record_sql($sql, array('userid' => $USER->id, 'courseid' => $course->id));
 if(!is_null($timecompleted->timecompleted))
-    certificate_print_text($pdf, $x + 35, $y + 103, 'L', $fontsans, '', 16, strftime('%d/%I/%Y', $timecompleted->timecompleted));
+    certificate_print_text($pdf, $x + 35, $y + 103, 'L', $fontsans, '', 16, strftime('%d/%m/%Y', $timecompleted->timecompleted));
 
 certificate_print_text($pdf, $x, $y + 92, 'C', $fontsans, '', 14,  certificate_get_date($certificate, $certrecord, $course));
 certificate_print_text($pdf, $x, $y + 102, 'C', $fontserif, '', 10, certificate_get_grade($certificate, $course));
